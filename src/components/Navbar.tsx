161673359cd19275ec2b34bf77555a480e7387f7
UtkarshGoel22 <utkarsh.goel22@gmail.com>
2024-02-11 16:14:48 +0530

    Handle user logout.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -30,6 +30,8 @@ import { ROUTES } from "@constants/routes.const"
 import { TEXT } from "@constants/text.const"
 import Tooltip from "@mui/material/Tooltip"
 
+import { authActions } from "@src/state/redux/authSlice"
+
 const drawerWidth = 240
 
 const openedMixin = (theme: Theme): CSSObject => ({
@@ -119,7 +121,7 @@ function Navbar() {
   }
 
   function handleClick() {
-    // TODO: dispatch logout action
+    dispatch(authActions.logoutUser(token))
   }
 
   const drawerItemsList = [
